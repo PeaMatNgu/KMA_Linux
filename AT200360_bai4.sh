7ffde2a1f7b6df46da5851a94839a3a5b32f62cd
PeaMatNgu <thanhtrungcaotran@gmail.com>
2026-01-06 15:16:59 +0000

    Update AT200360_bai4.sh

--- AT200360_bai4.sh
+++ AT200360_bai4.sh
@@ -1,14 +1,19 @@
+  GNU nano 7.2                                             bai2.sh                                                      
 #!/bin/bash
-sudo mkfs.ext4 /dev/sdb
-sudo mkdir -p /THI1_HK /THI2_HK
-sudo mount /dev/sdb /THI1_HK
-echo "Day la file test" | sudo tee /THI1_HK/test123.txt
-sudo rsync -av /THI1_HK/test123.txt /THI2_HK/
-sudo mkdir -p /THI21
-sudo cp /THI1_HK/test123.txt /THI21/
-sudo umount /THI1_HK
-echo "Ket qua kiem tra "
-sudo ls /THI1_HK
-echo "Ket qua dir THI2_HK la"
-sudo ls /THI2_HK
+lsblk
+mkfs.ext4 /dev/sdb
+mkdir -p /THI1_HK
+mkdir -p /THI2_HK
+mount /dev/sdb /THI1_HK
+echo "test thoi 123" > /THI1_HK/ok.txt
+rsync -av /THI1_HK/ok.txt /THI2_HK
+echo "KetquaTHI2"
+ls -l /THI2_HK/
+mkdir -p /THI21
+cp /etc/passwd /THI1_HK/
+umount /dev/sdb
+echo "Ketqua THI1"
+ls -l /THI1_HK/
+
+
 
