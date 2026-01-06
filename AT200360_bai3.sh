12a9a9a583c093363dbdf6fe648a8c680a680032
PeaMatNgu <thanhtrungcaotran@gmail.com>
2026-01-06 22:36:20 +0700

    Update AT200360_bai3.sh

--- AT200360_bai3.sh
+++ AT200360_bai3.sh
@@ -5,7 +5,7 @@ echo "U1:sv123456" | chpasswd
 echo "U2:sv123456" | chpasswd
 id U1
 id U2
-su - U1 -c "pwd"
+grep U1 /etc/passwd
 mkdir /home/U1/KMA_U1
 chown -R U1:U1 /home/U1/KMA_U1
 mv /home/U1/KMA_U1 /home/U2/
