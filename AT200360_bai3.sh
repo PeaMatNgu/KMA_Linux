b373b73fc1c37a52a3efac648513bfff448bb2c4
PeaMatNgu <thanhtrungcaotran@gmail.com>
2026-01-06 15:23:01 +0000

    Update AT200360_bai3.sh

--- AT200360_bai3.sh
+++ AT200360_bai3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-useradd -m  U1
+useradd -m U1
 useradd -m U2
 echo "U1:sv123456" | chpasswd
 echo "U2:sv123456" | chpasswd
@@ -7,7 +7,8 @@ id U1
 id U2
 su - U1 -c "pwd"
 mkdir /home/U1/KMA_U1
-chown U1:U1 /home/U1/KMA_U1
+chown -R U1:U1 /home/U1/KMA_U1
 mv /home/U1/KMA_U1 /home/U2/
 ls -l /home/U2/
+
 
