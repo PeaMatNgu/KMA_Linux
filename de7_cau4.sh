74867a761b000c8ff05904238fc2562c29c5d585
PeaMatNgu <thanhtrungcaotran@gmail.com>
2026-01-07 01:26:04 +0700

    Update de7_cau4.sh

--- de7_cau4.sh
+++ de7_cau4.sh
@@ -9,5 +9,5 @@ su - user123 -c "echo 'CaoTranThanhTrungAT200360' > /baitap/test.txt"
 chown user123:user456 /baitap/test.txt
 chmod 660 /baitap/test.txt
 su - user123 -c "ls -ld /baitap"
-tar -cvzf /baitap.tar.gz /baitap
+tar -cvzf baitap.tar.gz /baitap
 usermod -l 789user user456
