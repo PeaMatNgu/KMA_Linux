1c0822516a85c4052aed64a2b9b887e5dcd31e46
PeaMatNgu <thanhtrungcaotran@gmail.com>
2026-01-06 23:07:56 +0700

    Update bai4_de3.sh

--- bai4_de3.sh
+++ bai4_de3.sh
@@ -2,7 +2,8 @@
 ip addr add 192.168.0.100/255.255.255.0 dev ens33
 ip link set ens33 up
 ip addr show
-ping -c 3 localhost
+#Địa chỉ ipv4 của máy vật lý, cmd ipconfig ra
+ping -c 3 192.168.1.4
 sudo ip addr flush dev ens33
 ip addr add 192.168.1.200/255.255.255.252 dev ens33
 ip addr show ens33
