d9712b4e9b0cee613e1b92ba5199fe174fbcd45f
PeaMatNgu <thanhtrungcaotran@gmail.com>
2026-01-06 16:43:41 +0000

    Update de5_cau4.sh

--- de5_cau4.sh
+++ de5_cau4.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-useradd -m User123
-echo "User123:2711" | chpasswd
+useradd -m U10
+echo "U10:sv123" | chpasswd
 groupadd G123
-mkdir -p /KMA
-echo "check123aloalo" > /KMA/test2.txt
-ls -l /KMA/test2.txt
-chown User123:G123 /KMA/test2.txt
-chmod 550 /KMA/test2.txt
-ls -l /KMA/test2.txt
-su - User123 -c "gzip -c /KMA/test2.txt > /tmp/filenen.gz"
-ps -aux
+mkdir -p /KMA10
+echo "tao la bo nguyen vu" > /KMA10/test2.txt
+ls -l /KMA10
+chown :G123 /KMA10/test2.txt
+chmod 755 /KMA10/test2.txt
+ls -l /KMA10
+chown U10: /KMA10
+su - U10 -c "gzip -c /KMA10/test2.txt > /KMA10/filenen.gz"
+ls -l /KMA10
+ps -aux | head -n 10
+
+
